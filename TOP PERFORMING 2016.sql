1bb6691f3a5fe49836f1bd4a1bed23a4c66b1899
OkorigweClinton <clintonokorigwe@gmail.com>
2024-10-24 23:54:17 +0100

    Delete TOP PERFORMING 2016.sql

--- TOP PERFORMING 2016.sql
+++
@@ -1,6 +0,0 @@
-Select ProductKey, Region, SUM (OrderQuantity) as total_sales
-from AdventureWorks_Sales_2016
-where OrderDate BETWEEN '01/01/2016' and '12/31/2016'
-group by ProductKey,Region
-order by total_sales DESC
-LIMIT 1
